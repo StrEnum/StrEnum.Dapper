eb691b56d658f45e5819bf7abb20928473564a81
Dmitry Khmara <dmitry.khmara@gmail.com>
2022-08-01 18:01:50 +0300

    Add a generic StrEnum TypeHandler and a way to initialize it for all loaded string enums.

---
+++ src/StrEnum.Dapper/StrEnumDapper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using Dapper;
+
+namespace StrEnum.Dapper
+{
+    public static class StrEnumDapper
+    {
+        public static void UseStringEnums()
+        {
+            foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                var stringEnums = assembly.GetTypes().Where(o => o.IsStringEnum());
+
+                var genericTypeHandler = typeof(StrEnumTypeHandler<>);
+                
+                foreach (var stringEnumType in stringEnums)
+                {
+                    var handlerType = genericTypeHandler.MakeGenericType(stringEnumType);
+
+                    var handlerInstance = Activator.CreateInstance(handlerType);
+
+                    SqlMapper.AddTypeHandler(stringEnumType, handlerInstance as SqlMapper.ITypeHandler);
+                }
+            }
+        }
+    }
+}
